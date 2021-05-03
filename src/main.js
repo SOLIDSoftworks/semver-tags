89975b2a1773abf7bba58add4e49ff938567aa88
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2021-05-03 16:29:53 +0000

    adds lodash and searches for previous tag

--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
+const _ = require('lodash');
 
 async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
   const octokit = github.getOctokit(GITHUB_TOKEN);
   const { context = {} } = github;
 
-  let tags = await octokit.rest.repos.listTags({
+  let response = await octokit.rest.repos.listTags({
     ...context.repo
   });
 
-  console.log(tags);
+  let previous = _
+    .chain(response)
+    .map('name')
+    //.filter(name => )
+    .head()
+    .value()
+  ;
+
+  console.log(previous);
 }
 
 run();
