2be56c62c110f9e506444714ac8a611b2a184a93
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2021-05-03 16:44:40 +0000

    adds semver regex

--- dist/index.js
+++ dist/index.js
@@ -23275,10 +23275,18 @@ const github = __nccwpck_require__(998);
 const core = __nccwpck_require__(390);
 const _ = __nccwpck_require__(339);
 
+async function calculateNextVersion(previous) {
+  console.log(previous);
+  return '1.0.0';
+}
+
 async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const defaultVersion = core.getInput('default-version');
+  const versionPrefix = core.getInput('version-prefix');
   const octokit = github.getOctokit(GITHUB_TOKEN);
   const { context = {} } = github;
+  const pattern = new RegExp(`^${versionPrefix}(\d+)\.(\d+)\.(\d+)(-\w[\w\.]*)?(\+\w[\w\.]*)?$`, m);
 
   let response = await octokit.rest.repos.listTags({
     ...context.repo
@@ -23289,14 +23297,24 @@ async function run() {
     process.exit(1);
   }
 
+  let next = defaultVersion;
   let previous = _
     .chain(response.data)
     .map('name')
-    //.filter(name => )
+    .filter(name => pattern.test(name))
+    .map(name => {
+      return { name: name, matches: pattern.match(name) };
+    })
     .head()
     .value()
   ;
-
+  if(!previous) {
+    console.log(`No previous version tag found. Using '${ next }' as next version.`)
+  }
+  else {
+    console.log(`Previous version tag '${ previous.name }' found. Calculating next version.`)
+    next = await calculateNextVersion(previous);
+  }
   console.log(previous);
 }
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 const _ = require('lodash');
 
+async function calculateNextVersion(previous) {
+  console.log(previous);
+  return '1.0.0';
+}
+
 async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const defaultVersion = core.getInput('default-version');
+  const versionPrefix = core.getInput('version-prefix');
   const octokit = github.getOctokit(GITHUB_TOKEN);
   const { context = {} } = github;
+  const pattern = new RegExp(`^${versionPrefix}(\d+)\.(\d+)\.(\d+)(-\w[\w\.]*)?(\+\w[\w\.]*)?$`, m);
 
   let response = await octokit.rest.repos.listTags({
     ...context.repo
@@ -16,14 +24,24 @@ async function run() {
     process.exit(1);
   }
 
+  let next = defaultVersion;
   let previous = _
     .chain(response.data)
     .map('name')
-    //.filter(name => )
+    .filter(name => pattern.test(name))
+    .map(name => {
+      return { name: name, matches: pattern.match(name) };
+    })
     .head()
     .value()
   ;
-
+  if(!previous) {
+    console.log(`No previous version tag found. Using '${ next }' as next version.`)
+  }
+  else {
+    console.log(`Previous version tag '${ previous.name }' found. Calculating next version.`)
+    next = await calculateNextVersion(previous);
+  }
   console.log(previous);
 }
 
