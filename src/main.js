529a094f73f510ec4f421dd6aab65c24ae6bac28
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2022-09-22 12:45:08 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,20 +2,23 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 const _ = require('lodash');
 
-
-const tagPrefix = core.getInput('tag-prefix');
-const versionPattern = new RegExp(`^${tagPrefix}(\\d+)\\.(\\d+)\\.(\\d+)(-(\\w[\\w\.]*))?(\\+(\\w[\\w\\.]*))?$`, 'm');
 const nextVersion = function(semver, major, minor) {
   this.semver = semver;
   this.major = major;
   this.minor = minor;
 };
 
+function generateVersionPattern(tagPrefix) {
+  return new RegExp(`^${tagPrefix}(\\d+)\\.(\\d+)\\.(\\d+)(-(\\w[\\w\.]*))?(\\+(\\w[\\w\\.]*))?$`, 'm');
+}
+
 async function calculateNextVersion(previous) {
   const defaultVersion = core.getInput('default-version');
   const incrementedValue = core.getInput('incremented-value');
   const prerelease = core.getInput('prerelease');
   const metadata = core.getInput('metadata');
+  const tagPrefix = core.getInput('tag-prefix');
+  const versionPattern = generateVersionPattern(tagPrefix);
 
   let semanticVersion = '';
   let majorVersion = '';
@@ -83,6 +86,8 @@ async function run() {
   const addMinorTag = core.getInput('add-minor-tag');
   const addMajorTag = core.getInput('add-major-tag');
   const prerelease = !!core.getInput('prerelease');
+  const tagPrefix = core.getInput('tag-prefix');
+  const versionPattern = generateVersionPattern(tagPrefix);
 
   const octokit = github.getOctokit(GITHUB_TOKEN);
   const { context = {} } = github;
